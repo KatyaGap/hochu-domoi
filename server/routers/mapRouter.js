fd70f3e26f864a601f2b5837d5ec13f9ec343365
KatyaGap <katyakate22@mail.ru>
2022-07-11 14:30:20 +0300

    Array of images

--- server/routers/mapRouter.js
+++ server/routers/mapRouter.js
@@ -74,24 +74,27 @@ router
     console.log('REQ FILE------------------------', req.files);
     try {
       // const type = await Type.findOne({ where: { type: req.params.type } });
-
       const { userId } = req.session;
-      await Post.create({
-        text: req.body.text,
-        pet_id: req.body.pet_id,
-        type_id: req.body.type_id,
-        status_id: req.body.status_id,
-        breed_id: req.body.breed_id || 1,
-        color_id: req.body.color_id,
-        size_id: req.body.size,
-        // lost_date: req.body.date || '2020-01-17T04:33:12.000Z',   ПОТОМ РАСКОММЕНТИТЬ
-        lost_date: req.body.date, // НУЖНА ФОРМА НА ФРОНТЕ С ДАТОЙ ПОТЕРИ
-        address_string: req.body.address_string || 'Moscow', // ДАННЫЕ ДОЛЖНЫ ИЗ КАРТЫ ТЯНУТЬСЯ
-        address_lattitude: req.body.address_lattitude || 55.683986493805385, // ДАННЫЕ ДОЛЖНЫ ИЗ КАРТЫ ТЯНУТЬСЯ
-        address_longitude: req.body.address_longitude || 37.534586242675786, // ДАННЫЕ ДОЛЖНЫ ИЗ КАРТЫ ТЯНУТЬСЯ
-        // photo_url: req.file.path.replace('public', ''),
-        user_id: userId,
-      });
+      const arr = req.files;
+      arr.map(
+        await ((img, i) =>
+          Post.create({
+            text: req.body.text,
+            pet_id: req.body.pet_id,
+            type_id: req.body.type_id,
+            status_id: req.body.status_id,
+            breed_id: req.body.breed_id || 1,
+            color_id: req.body.color_id,
+            size_id: req.body.size,
+            // lost_date: req.body.date || '2020-01-17T04:33:12.000Z',   ПОТОМ РАСКОММЕНТИТЬ
+            lost_date: req.body.date, // НУЖНА ФОРМА НА ФРОНТЕ С ДАТОЙ ПОТЕРИ
+            address_string: req.body.address_string || 'Moscow', // ДАННЫЕ ДОЛЖНЫ ИЗ КАРТЫ ТЯНУТЬСЯ
+            address_lattitude: req.body.address_lattitude || 55.683986493805385, // ДАННЫЕ ДОЛЖНЫ ИЗ КАРТЫ ТЯНУТЬСЯ
+            address_longitude: req.body.address_longitude || 37.534586242675786, // ДАННЫЕ ДОЛЖНЫ ИЗ КАРТЫ ТЯНУТЬСЯ
+            photo_url: arr[i].path.replace('public', ''),
+            user_id: userId,
+          }))
+      );
       res.json({ text: 'Круто!' }); // тупо строка для теста. Потом поменять на что-то правильное
     } catch (error) {
       console.log(error);
