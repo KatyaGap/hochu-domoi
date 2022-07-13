512a44f47807398785e815c467801f8629c0cb85
Marat Kuzakhmetov <maratkuzakhmetov@mail.ru>
2022-07-13 18:22:32 +0300

    Фильтр работает с костылем, немного криво, но работает

--- client/src/components/elements/PostList.js
+++ client/src/components/elements/PostList.js
@@ -1,34 +1,45 @@
 import { ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
 import { Box } from '@mui/system';
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { deletePostThunk, getAdvertsThunk } from '../../redux/actions/adverts';
+import { getAdvertsThunk } from '../../redux/actions/adverts';
 
-function PostList({ adverts }) {
+function PostList({ adverts, posts = [] }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  // const handleDelete = useCallback((id) => {
-  //   dispatch(deletePostThunk(id));
-  // }, []);
+
+  const [flag, setFlag] = useState(false);
+  if (posts.length !== 0 && !flag) {
+    setFlag(true);
+  }
+
+  console.log('FLAG', posts);
+
+  const renderList = (array) => array.map((item, ind) => (
+    <ImageListItem key={ind + 1} onClick={() => navigate(`/pet/${item.id}`)}>
+      <img
+        src={`${item.photo_url}?w=248&fit=crop&auto=format`}
+        srcSet={`${item.photo_url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+        alt={item.title}
+        loading="lazy"
+      />
+
+      <ImageListItemBar
+        title={`${item.text?.slice(0, 25)}...`}
+          // subtitle={<span>{item.text}</span>}
+        position="below"
+      />
+    </ImageListItem>
+  ));
+
   return (
     <ImageList sx={{ width: 600 }} cols={4} gap={8}>
-      {adverts?.map((item, ind) => (
-        <ImageListItem key={ind + 1} onClick={() => navigate(`/pet/${item.id}`)}>
-          <img
-            src={`${item.photo_url}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.photo_url}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-
-          <ImageListItemBar
-            title={`${item.text?.slice(0, 25)}...`}
-            // subtitle={<span>{item.text}</span>}
-            position="below"
-          />
-        </ImageListItem>
-      ))}
+      {posts.length && renderList(posts)}
+      {!posts.length && flag && <div>По данным критериям посты не найдены</div>}
+      {!posts.length && !flag && renderList(adverts)}
+      {/* {posts.length ? renderList(posts) : renderList(adverts)}
+      {!posts.length && <div>По данным критериям посты не найдены</div>} */}
     </ImageList>
   );
 }
