c2d4804520e4c1266eabe9d8a2a74886916bf259
KatyaGap <katyakate22@mail.ru>
2022-07-14 15:48:54 +0300

    server new change

--- client/src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from '@redux-devtools/extension';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/root';
-
-export default createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
