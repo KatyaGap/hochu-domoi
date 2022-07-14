44997d665398021a20c151dba38871aed5733726
KatyaGap <katyakate22@mail.ru>
2022-07-14 18:34:02 +0300

    fix

--- db/database.js
+++ db/database.js
@@ -20,9 +20,9 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: '127.0.0.1',
+    host: process.env.DB_HOST,
     dialect: 'postgres',
-    dialiectOptions: {
+		dialiectOptions: {
       ssl: { rejectUnauthorized: false },
     },
   },
